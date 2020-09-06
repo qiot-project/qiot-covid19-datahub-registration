da422d59d5c03751f118ae184a4d2ba95e5ce73c
abattagl <abattagl@192.168.178.22>
2020-09-06 20:02:00 +0200

    station data required via path param

--- src/main/java/com/redhat/qiot/datahub/endpoint/rest/RegisterResource.java
+++ src/main/java/com/redhat/qiot/datahub/endpoint/rest/RegisterResource.java
@@ -1,19 +1,13 @@
 package com.redhat.qiot.datahub.endpoint.rest;
 
-import java.io.StringReader;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.slf4j.Logger;
@@ -32,41 +26,59 @@ public class RegisterResource {
     @Inject
     DataStoreService dsService;
 
+    // @PUT
+    // public int register(@QueryParam("stationData") String stationData) {
+    // if (LOGGER.isDebugEnabled()) {
+    // LOGGER.debug("register(String) - start");
+    // }
+    //
+    // String serial, name;
+    // double longitude, latitude;
+    //
+    // JsonObject jsonObject = null;
+    // try (JsonReader reader = Json
+    // .createReader(new StringReader(stationData));) {
+    // jsonObject = reader.readObject();
+    // serial = jsonObject.getString("serial");
+    // name = jsonObject.getString("name");
+    // longitude = jsonObject.getJsonNumber("lon").doubleValue();
+    // latitude = jsonObject.getJsonNumber("lat").doubleValue();
+    // }
+    // int returnint = dsService.register(serial, name, longitude, latitude);
+    // if (LOGGER.isDebugEnabled()) {
+    // LOGGER.debug("register(String) - end");
+    // }
+    // return returnint;
+    // }
+
     @PUT
-    public int register(@QueryParam("stationData") String stationData) {
+    @Path("/serial/{serial}/name/{name}/longitude/{longitude}/latitude/{latitude}")
+    public int register(@PathParam("serial") String serial,
+            @PathParam("name") String name,
+            @PathParam("longitude") double longitude,
+            @PathParam("latitude") double latitude) {
         if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("register(String) - start"); //$NON-NLS-1$
+            LOGGER.debug("register(String) - start");
         }
 
-        String serial, name;
-        double longitude, latitude;
-
-        JsonObject jsonObject = null;
-        try (JsonReader reader = Json
-                .createReader(new StringReader(stationData));) {
-            jsonObject = reader.readObject();
-            serial = jsonObject.getString("serial");
-            name = jsonObject.getString("name");
-            longitude = jsonObject.getJsonNumber("lon").doubleValue();
-            latitude = jsonObject.getJsonNumber("lat").doubleValue();
-        }
         int returnint = dsService.register(serial, name, longitude, latitude);
         if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("register(String) - end"); //$NON-NLS-1$
+            LOGGER.debug("register(String) - end");
         }
         return returnint;
     }
 
     @DELETE
-    public void unregister(@QueryParam("id") int id) {
+    @Path("/id/{id}")
+    public void unregister(@PathParam("id") int id) {
         if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("unregister(int) - start"); //$NON-NLS-1$
+            LOGGER.debug("unregister(int) - start");
         }
 
         dsService.unregister(id);
 
         if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("unregister(int) - end"); //$NON-NLS-1$
+            LOGGER.debug("unregister(int) - end");
         }
     }
 }
