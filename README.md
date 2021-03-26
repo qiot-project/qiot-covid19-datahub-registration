87463832bea0fde0a233138a3ceb0a62ebee52f8
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-03-26 11:31:54 +0100

    updated build script and readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# qiot-datahub-registration
+# registration
+
+A microservice exposed to the IoT devices over secured http.
+
+The connection to the service is secured through the implementation of a mutual authentication mechanism making sure only certified devices have access to this service and vice-versa.
+
+Scope of this microservice is to accept incoming registration requests from the devices provision certificates using the vault and generate a unique registration ID for the devide.
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 chmod +x mvn*
 ./mvnw clean package -Pnative  -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
-docker rmi quay.io/qiotcovid19/qiot-datahub-registration:2.0.0-alpha --force
-docker build -f src/main/docker/Dockerfile.native -t quay.io/qiotcovid19/qiot-datahub-registration:2.0.0-alpha .
-docker push quay.io/qiotcovid19/qiot-datahub-registration:2.0.0-alpha
+docker rmi quay.io/qiotcovid19/qiot-datahub-registration:2.0.0-beta --force
+docker build -f src/main/docker/Dockerfile.native -t quay.io/qiotcovid19/qiot-datahub-registration:2.0.0-beta .
+docker push quay.io/qiotcovid19/qiot-datahub-registration:2.0.0-beta
 #docker run -it --rm -p 5000:5000 --net host quay.io/qiot/qiot-datahub-registration
